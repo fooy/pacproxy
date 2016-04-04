a459ed717760e9736ec1aeb60887e3816cfe1f5d
fuy <fooy84@gmail.com>
2016-04-04 20:53:50 +0800

    fix shExpMatch

--- pac.js
+++ pac.js
@@ -1,53 +1,60 @@
-exports.shExpMatch = function (url,pattern) {
-    var pCharCode;
-    var isAggressive = false;
-    var pIndex;
-    var urlIndex = 0;
-    var lastIndex;
-    var patternLength = pattern.length;
-    var urlLength = url.length;
-    for (pIndex = 0; pIndex < patternLength; pIndex += 1) {
-      pCharCode = pattern.charCodeAt(pIndex); // use charCodeAt for performance, see http://jsperf.com/charat-charcodeat-brackets
-      if (pCharCode === 63) { // use if instead of switch for performance, see http://jsperf.com/switch-if
-        if (isAggressive) {
-          urlIndex += 1;
+function shExpMatch(url,pattern) {
+    var plen = pattern.length;
+    var nlen = url.length;
+    for (var pidx = 0,nidx=0; pidx < plen; pidx++,nidx++ ) {
+        var p = pattern.charCodeAt(pidx); 
+        switch ( p ) {
+            case 63: //?
+                if(nidx >= nlen) return false;
+                break;
+            case 42: //*
+                if(pidx === plen -1 ) return true;
+                for( p = pattern.charCodeAt(++pidx) ;( p === 42 || p === 63 ) && (pidx < plen) ; p = pattern.charCodeAt(++pidx) ) {
+                    if( p === '?' ){
+                        if( nidx == nlen -1 ) return false;
+                        else nidx++ ;
+                    }
+                }
+                for ( ; nidx<nlen ; nidx++ ){
+                    if ( p === url.charCodeAt(nidx) && shExpMatch( url.substr(nidx), pattern.substr(pidx) ) )
+                        return true;
+                }
+                return false;
+                break;
+            default:
+                if ( p != url.charCodeAt(nidx) )
+                    return false;
+                break;
         }
-        isAggressive = false;
-        urlIndex += 1;
-      } else if (pCharCode === 42) {
-        if (pIndex === patternLength - 1) {
-          return urlIndex <= urlLength;
-        } else {
-          isAggressive = true;
-        }
-      } else {
-        if (isAggressive) {
-          lastIndex = urlIndex;
-          urlIndex = url.indexOf(String.fromCharCode(pCharCode), lastIndex + 1);
-          if (urlIndex < 0) {
-            if (url.charCodeAt(lastIndex) !== pCharCode) {
-              return false;
-            }
-            urlIndex = lastIndex;
-          }
-          isAggressive = false;
-        } else {
-          if (urlIndex >= urlLength || url.charCodeAt(urlIndex) !== pCharCode) {
-            return false;
-          }
-        }
-        urlIndex += 1;
-      }
     }
-    return urlIndex === urlLength;
+    return nidx === nlen;
 }
+
+exports.shExpMatch = shExpMatch ;
 
 exports.dnsDomainIs = function (host,domain) {
-
 }
-
+exports.dateRange= function (start,end) {
+}
+exports.weekdayRange =function( start,end) {
+}
 exports.isInNet = function (ip,net,mask) {
-    return 2 ;
 }
 exports.myIpAddress = function () {
+}
+
+function tests( t, p ,r) {
+    var cases = [
+        [ "hhh.abc.com/sfjkdjksfd","*?c*?com*", true ],
+        [ "abc.1.com/", "*abc.*.com/*", true ],
+        [ "abc.1.com/adfs","*abc.*.com/*" , true ],
+        [ "abc.1.com/adfs","abc.1.com/adf?" ,true],
+        [ "abc.1.com/adf","abc.1.com/adf?" ,false],
+        [ "abc.1.com/adf","",false] ];
+    for ( var k in cases )
+    {
+        var i= cases[k];
+        var r=shExpMatch ( i[0], i[1] )
+        console.log (i[0] + " ~ " + i[1] +"\t"+r  + "-> " + ( r=== i[2] ? "pass":"error" ))
+    }
 }
